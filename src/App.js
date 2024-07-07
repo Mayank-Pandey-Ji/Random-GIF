31d195d5eb8244d8eee4e6dc8cb6334c025a0a7f
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2024-07-07 10:51:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Random from './components/Random'
+import Tag from './components/Tag'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-screen flex flex-col background relative overflow-hidden'>
+      <h1 className='bg-white absolute w-full text-center font-bold text-4xl rounded-lg mt-[40px] ml-[25px] px-10 py-2 '>RANDOM GIFS</h1>
+      <div className='flex flex-col'>
+        <Random/>
+        <Tag/>
+      </div>
     </div>
   );
 }
