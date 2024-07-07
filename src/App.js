c19628e33d87bbd3525b147ec4f2199e05326acc
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2024-07-07 16:00:53 +0530

    api key added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Random from './components/Random'
 import Tag from './components/Tag'
 function App() {
   return (
-    <div className='w-full h-screen flex flex-col background relative overflow-hidden'>
-      <h1 className='bg-white absolute w-full text-center font-bold text-4xl rounded-lg mt-[40px] ml-[25px] px-10 py-2 '>RANDOM GIFS</h1>
-      <div className='flex flex-col'>
+    <div className='w-full h-screen flex flex-col background relative overflow-hidden items-center'>
+      <h1 className='bg-white  w-11/12 text-center font-bold text-4xl rounded-lg mt-[40px] px-10 py-2 '>RANDOM GIFS</h1>
+      <div className='flex flex-col w-full items-center gap-y-10 mt-[30px]'>
         <Random/>
         <Tag/>
       </div>
--- src/components/Random.js
+++ src/components/Random.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Random = () => {
+
+  const {gif , setGif} = useState("");
+  function clickHandler()
+  {
+
+  }
+
   return (
-    <div>
-      
+    <div className=' w-1/2  h-[450px] bg-[#4ade80] rounded-xl  border-gray-600 border-2 flex flex-col items-center gap-y-5 mt-[15px]'>
+      <h1 className='text-2xl font-bold underline'> A RANDOM GIF</h1>
+      <img src={gif} alt="" />
+      <button onClick={clickHandler} className='w-10/12 bg-[#c9f5d9] text-lg py-2 rounded-lg'>Generate</button>
     </div>
   )
 }
